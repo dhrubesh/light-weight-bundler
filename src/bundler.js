eeb5b261874ad369563c91c910546e970cd66bcc
Dhrubesh Deb Sharma <dhrubesh97@gmail.com>
2019-12-21 17:41:00 +0530

    adds a func to get dependencies and stuff from the filePath

---
+++ src/bundler.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const parser = require("@babel/parser");
+const traverse = require("@babel/traverse").default;
+const babel = require("@babel/core");
+
+function getModuleInfo(filePath) {
+    // read the file
+    const content = fs.readFileSync(filePath, "utf-8");
+
+    // create abstract syntax tree
+    const ast = parser.parse(content, {
+        sourceType: "module"
+    });
+
+    // dependencies in the module
+    const deps = [];
+
+    // traverse through the ast
+    traverse(ast, {
+        ImportDeclaration: ({ node }) => {
+            deps.push(node.source.value);
+        }
+    });
+
+    const { code } = babel.transformFromAstSync(ast, null);
+
+    return {
+        filePath,
+        deps,
+        code
+    };
+}
+
+let testEntry = getModuleInfo('./test-project/index.js');
+
+console.log('testEntry', testEntry);
